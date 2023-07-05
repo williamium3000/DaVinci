19900e9519dc49e92d9f2b998398b225e287314f
william <liyijiang3000@163.com>
2023-07-05 17:17:14 -0400

    fix bug

--- tools/dist_ve.sh
+++ tools/dist_ve.sh
@@ -5,5 +5,5 @@ ckpt=work_dirs/davinci/pretrain_coco_vg/checkpoint_39.pth
 mkdir -p $save_path
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port 29509 --use_env VE.py --config $cfg \
---output_dir $save_path --amp \
+--output_dir $save_path \
 --checkpoint $ckpt 2>&1 | tee $save_path/$now.txt
