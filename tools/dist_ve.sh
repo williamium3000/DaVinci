db53f876349c0208bca2b9f7b4aaf36f4d5317fe
william <liyijiang3000@163.com>
2023-07-13 14:27:51 +0800

    fix bug

--- tools/dist_ve.sh
+++ tools/dist_ve.sh
@@ -1,7 +1,7 @@
 now=$(date +"%Y%m%d_%H%M%S")
 save_path=work_dirs/pretrain_coco_vg/ve_davinci_cfg
 cfg=configs/VE.yaml
-ckpt=/root/william/project/explore-vlp/work_dirs/davinci/pretrain_coco_vg_10e/checkpoint_09.pth
+ckpt=work_dirs/pretrain_coco_vg/checkpoint_39.pth
 mkdir -p $save_path
 
 CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 29506 --use_env VE.py --config $cfg \
