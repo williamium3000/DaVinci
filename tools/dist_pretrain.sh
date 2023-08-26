5b7b299adf331c1076bf02a5ac6f55760e2184f3
yijiang Li <yli7@jhpce01.cm.cluster>
2023-08-26 01:03:16 -0400

    fix bug

--- tools/dist_pretrain.sh
+++ tools/dist_pretrain.sh
@@ -1,9 +1,9 @@
 now=$(date +"%Y%m%d_%H%M%S")
-save_path=work_dirs/pretrain_coco_vg_c4_10e
+save_path=work_dirs/pretrain_coco_vg_c4_5e_acc2_4gpu
 mkdir -p $save_path
-python3 -m torch.distributed.launch --nproc_per_node=8  \
+python3 -m torch.distributed.launch --nproc_per_node=4  \
     --master_port=39587 \
     --use_env Pretrain.py \
-    --config ./configs/Pretrain_10e.yaml \
+    --config configs/Pretrain_5e_semi_acc2_4gpu.yaml \
     --amp \
     --output_dir $save_path 2>&1 | tee $save_path/$now.txt
--- tools/dist_pretrain_wo_c4.sh
+++ tools/dist_pretrain_wo_c4.sh
@@ -1,8 +1,8 @@
 now=$(date +"%Y%m%d_%H%M%S")
-save_path=work_dirs/test 
+save_path=work_dirs/pretrain_vg_coco_wo_c4_10e_acc2_4gpu 
 mkdir -p $save_path
-python3 -m torch.distributed.launch --nproc_per_node=8  \
+python3 -m torch.distributed.launch --nproc_per_node=4  \
     --master_port=39587 \
     --use_env Pretrain_wo_c4.py \
-    --config ./configs/Pretrain.yaml \
+    --config configs/Pretrain_10e_semi_acc2_4gpu.yaml \
     --output_dir $save_path 2>&1 | tee $save_path/$now.txt
